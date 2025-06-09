bca751fb9bcf7d20b236151db42da706a6cc8e21
jassekho <jasdipsekhon@gmail.com>
2025-06-08 17:28:35 -0700

    added boilerplate for sensor

--- Drivers/Sensors/bmi088.c
+++ Drivers/Sensors/bmi088.c
@@ -1,1 +1,7 @@
-#pragma once
+/**
+ * @file        bmi088.c
+ * @brief       Driver for BMI088 IMU sensor
+ * @author      Jasdip Sekhon
+ * @date        2025
+ * @license     SPDX-License-Identifier: MIT
+ */
--- Drivers/Sensors/bmi088.h
+++ Drivers/Sensors/bmi088.h
@@ -1,1 +1,15 @@
 #pragma once
+
+/**
+ * @file        bmi0888.h
+ * @brief       Driver for BMI088 IMU sensor
+ * 
+#pragma once
+
+/**
+ * @file        bmi088.h
+ * @brief       Driver for BMI088 IMU sensor
+ * @author      Jasdip Sekhon
+ * @date        2025
+ * @license     SPDX-License-Identifier: MIT
+ */
