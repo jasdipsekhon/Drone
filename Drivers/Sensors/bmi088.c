63b1614ec3b6f83c060d687dda73f3ee1f42810f
jassekho <jasdipsekhon@gmail.com>
2025-06-08 18:05:00 -0700

    added sensor code

--- Drivers/Sensors/bmi088.c
+++ Drivers/Sensors/bmi088.c
@@ -5,3 +5,19 @@
  * @date        2025
  * @license     SPDX-License-Identifier: MIT
  */
+
+#include "bmi088.h"
+
+extern SPI_HandleTypeDef hspi1;
+
+static void bmi088_cs_accel_low(void)  { HAL_GPIO_WritePin(BMI088_ACCEL_CS_GPIO, BMI088_ACCEL_CS_PIN, GPIO_PIN_RESET); }
+static void bmi088_cs_accel_high(void) { HAL_GPIO_WritePin(BMI088_ACCEL_CS_GPIO, BMI088_ACCEL_CS_PIN, GPIO_PIN_SET); }
+
+static void bmi088_cs_gyro_low(void)   { HAL_GPIO_WritePin(BMI088_GYRO_CS_GPIO, BMI088_GYRO_CS_PIN, GPIO_PIN_RESET); }
+static void bmi088_cs_gyro_high(void)  { HAL_GPIO_WritePin(BMI088_GYRO_CS_GPIO, BMI088_GYRO_CS_PIN, GPIO_PIN_SET); }
+
+bool bmi088_init(void) {
+}
+
+bool bmi088_read_data(bmi088_data_t *data) {
+}
--- Drivers/Sensors/bmi088.h
+++ Drivers/Sensors/bmi088.h
@@ -7,3 +7,23 @@
  * @date        2025
  * @license     SPDX-License-Identifier: MIT
  */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "stm32h7xx_hal.h"
+
+typedef struct {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+} bmi088_vector_t;
+typedef struct {
+    bmi088_vector_t accel;
+    bmi088_vector_t gyro;
+    float temperature;
+} bmi088_data_t;
+
+bool bmi088_init(void);
+bool bmi088_read_accel(bmi088_vector_t *accel);
+bool bmi088_read_gyro(bmi088_vector_t *gyro);
+bool bmi088_read_temperature(float *temp);
